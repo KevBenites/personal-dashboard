8ee4fce5e2087e6c0613efa5216743727ca53123
Kevin Benites Vega <kevin.benitesv@gmail.com>
2026-05-01 14:05:24 -0500

    modificacion de diseño de Login

--- src/features/auth/hooks/use-auth.js
+++ src/features/auth/hooks/use-auth.js
@@ -1,18 +1,24 @@
+import { useState } from 'react';
 import { loginService } from '../services/auth-service';
 import { useAuthStore } from '../store/auth-store';
 
 export const useAuth = () => {
   const { token, setToken, logout } = useAuthStore();
+  const [isLoading, setIsLoading] = useState(false);
 
   const login = async ({ username, password }) => {
+    setIsLoading(true);
     const token = await loginService({ username, password });
     setToken(token);
+    setIsLoading(false);
   };
 
   return {
     token,
     login,
     logout,
+    isLoading,
+    setIsLoading,
     isAuthenticated: !!token,
   };
 };
--- src/features/auth/pages/login/login.jsx
+++ src/features/auth/pages/login/login.jsx
@@ -3,7 +3,7 @@ import { useAuth } from '../../hooks/use-auth';
 import { useState } from 'react';
 
 export const LoginPage = () => {
-  const { login } = useAuth();
+  const { login, isLoading, setIsLoading } = useAuth();
   const navigate = useNavigate();
 
   const [form, setForm] = useState({
@@ -19,6 +19,11 @@ export const LoginPage = () => {
       navigate('/');
     } catch {
       alert('Username or password are incorrect');
+      setIsLoading(false);
+      setForm({
+        username: '',
+        password: '',
+      });
     }
   };
 
@@ -27,24 +32,32 @@ export const LoginPage = () => {
       className="min-h-screen flex justify-center items-center"
       onSubmit={handleSubmit}
     >
-      <div className="h-105 w-fit p-4 flex flex-col justify-center items-center gap-6">
-        <h1>Login</h1>
+      <div className="h-105 w-fit py-4 px-6 flex flex-col justify-center items-center gap-6 border-2 border-gray-500 rounded-2xl">
+        <h1 className="text-3xl font-bold">Login</h1>
+        <h2 className="text-base text-black/70 font-semibold">
+          Bienvenido a la TodoApp
+        </h2>
 
         <input
           type="text"
           placeholder="username"
-          className="w-2xs focus:outline-white/50 focus:outline-none px-4 border border-slate-700 rounded-lg"
+          value={form.username}
+          className="w-2xs focus:outline-white/50 focus:outline-none px-4 py-1 border border-slate-700 rounded-lg"
           onChange={(e) => setForm({ ...form, username: e.target.value })}
         />
         <input
           type="password"
           placeholder="password"
-          className="w-2xs focus:outline-white/50 focus:outline-none px-4 border border-slate-700 rounded-lg"
+          value={form.password}
+          className="w-2xs focus:outline-white/50 focus:outline-none px-4 py-1 border border-slate-700 rounded-lg"
           onChange={(e) => setForm({ ...form, password: e.target.value })}
         />
 
-        <button className="px-3 py-2 bg-slate-900 text-white rounded-lg cursor-pointer">
-          Submit
+        <button
+          className={`px-5 py-2 bg-slate-900 text-white rounded-lg cursor-pointer ${isLoading ? 'disabled:bg-red-500/80' : ''}`}
+          disabled={isLoading}
+        >
+          {isLoading ? 'Loading' : 'Submit'}
         </button>
       </div>
     </form>
